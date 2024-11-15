9f0359998889290735d51b673c2374a2983fac8c
Asignment <mdashadahamed25@gmail.com>
2024-11-15 22:43:39 +0600

    V2.1.1

--- lib/Home.dart
+++ lib/Home.dart
@@ -6,7 +6,34 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Ashadur Rahman'),),
+      appBar: AppBar(
+        title: Center(
+            child: Text(
+          'Need Blood',
+          style: TextStyle(color: Colors.white),
+        )),
+        backgroundColor: Colors.red,
+        actions: [
+          IconButton(onPressed: (){}, icon: Icon(Icons.add,color: Colors.white,))
+        ],
+
+      ),
+
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              backgroundColor: Colors.grey,
+              maxRadius: 50,
+              minRadius: 50,
+              child: Icon(Icons.bloodtype_outlined,color: Colors.red,size: 50,),
+            ),
+            Text('Donate Blood',style: TextStyle(fontWeight: FontWeight.w800),),
+          ],
+        ),
+      ),
+
     );
   }
 }
